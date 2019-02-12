b4cb5bce1418ac14686d7aa9e888f4bcaa933b99
Andy Young <ayscript@gmail.com>
2019-02-12 13:36:26 -0700

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use std::{i8, i16, i32, i64, u8, u32, u64, isize, usize, f32, f64 };
+
+use std::io::stdin;
+
+fn main() {
+
+    // let mut neg_4 = -4i32;
+
+    // println!("abs(-4) = {}", neg_4.abs());
+    // println!("4 ^ 6 = {}", 4i32.pow(6));
+
+    // println!("sqrt 9 = {}", 9f64.sqrt());
+    // println!("cbrt 27 = {}", 27f64.cbrt());
+
+    // println!("Round 1.45 = {}", 1.45f64.round());
+    // println!("Floor 1.45 = {}", 1.45f64.floor());
+
+    // println!("Ceiling 1.45 = {}", 1.45f64.ceil());
+    // println!("e ^ 2 = {}", 2f64.exp());
+
+    // println!("log(2) = {}", 2f64.ln());
+    // println!("log10(2) = {}", 2f64.log10());
+
+    // println!("90 to radians = {}", 90f64.to_radians());
+    // println!("PI to Degrees = {}", 3.14f64.to_degrees());
+
+    // println!("Max 4, 5 = {}", 4f64.max(5f64));
+    // println!("Min 4, 5 = {}", 4f64.min(5f64));
+
+    // println!("Sin 3.14 = {}", 3.14f64.sin());
+    // println!("Cosine 3.14 = {}", 3.14f64.cos());
+
+    // println!("5 + 4 = {}", 5 + 4);
+    // println!("5 - 4 = {}", 5 - 4);
+    // println!("5 * 4 = {}", 5 * 4);
+    // println!("5 / 4 = {}", 5 / 4);
+    // println!("5 % 4 = {}", 5 % 4);
+
+
+    // let is_it_true: bool = true;
+
+    // let let_x: char = 'x';
+
+    // println!("It is {0} that {1} is {0}", is_it_true, let_x);
+
+    // println!("{:.2}", 1.234);
+
+    // println!("B: {:b} H: {:x} O: {:o}", 10, 10, 10);
+
+    // println!("{ten:>ws$}", ten=10, ws=5);
+    // println!("{ten:>0ws$}", ten=10, ws=5);
+
+    // let num = 10;
+
+    // let mut age: i32 = 39;
+
+
+    // println!("I am {} years old", age);
+
+    // let (f_name, l_name) = ("Andy", "Young");
+
+    // println!("Max i8 {}", i8::MAX);
+    // println!("Min i8 {}", i8::MIN);
+
+    // println!("Max i16 {}", i16::MAX);
+    // println!("Min i16 {}", i16::MIN);
+
+    // println!("Max i32 {}", i32::MAX);
+    // println!("Min i32 {}", i32::MIN);
+
+    // println!("Max i64 {}", i64::MAX);
+    // println!("Min i64 {}", i64::MIN);
+
+    // println!("Max isize {}", isize::MAX);
+    // println!("Min isize {}", isize::MIN);
+
+    // println!("Max usize {}", usize::MAX);
+    // println!("Min usize {}", usize::MIN);
+
+    // println!("MAX f32 {}", f32::MAX);
+    // println!("MIN f32 {}", f32::MIN);
+
+    // println!("MAX f64 {}", f64::MAX);
+    // println!("MIN f64 {}", f64::MIN);
+}
